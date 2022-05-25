34f692f5da374f7a54d41c3a285b2b2c3746e32d
PrzemekLewtak <przemek.lewtak@heywood.co.uk>
2022-05-25 23:41:33 +0100

    Created static data file with types

---
+++ src/types/documents.ts
@@ -0,0 +1,19 @@
+
+export type TypeOne = File | Folder;
+
+export type File = {
+    id?: string,
+    type: "doc" | "pdf" | "csv" | "mov",         
+    name: string,
+    added: string,
+}
+
+export type Folder = {
+    id?: string;
+    type: "folder",     
+    name: string,
+    files: File[],
+    added: string,
+}
+
+
