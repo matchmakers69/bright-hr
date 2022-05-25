7796ae867a310b6ef8f52e57bacb6ae28aa66249
PrzemekLewtak <przemek.lewtak@heywood.co.uk>
2022-05-25 23:34:43 +0100

    Created components for svg icons

---
+++ src/components/ui/Svg/index.tsx
@@ -0,0 +1,5 @@
+export { default as PdfIcon } from "./PdfIcon";
+export { default as FolderIcon } from "./FolderIcon";
+export { default as CsvIcon } from "./CsvIcon";
+export { default as DocIcon } from "./DocIcon";
+export { default as MovIcon } from "./MovIcon";
