5be7b4f32d74425f19d29f431186b2b06a2cd377
PrzemekLewtak <przemek.lewtak@heywood.co.uk>
2022-06-19 10:49:49 +0100

    Updated sorted by name method by adding recursive mode

--- src/features/Documents/services/service.ts
+++ src/features/Documents/services/service.ts
@@ -6,31 +6,65 @@
 import { Documents } from "types/documents";
 import { SortType } from "../DocumentsList";
 
-    export const sortDocuments = (documents: Documents, sortType: SortType, asc: boolean): Documents => {
-        if(sortType === "name") {
-          const sorted = [...documents].sort((a, b) => a.name.localeCompare(b.name));
-          
-          return asc ? sorted : sorted.reverse()
-        }
+export const sortDocuments = (documents: Documents, sortType: SortType, asc: boolean): Documents => {
+  if(sortType === "name") {
+    const sorted = [...documents].sort((a, b) => a.name.localeCompare(b.name));
+    const s = asc ? sorted : sorted.reverse();
 
-        if(sortType === "size") {
-          const sorted = [...documents].sort((a, b) => {
-            if(a.type === "folder" && b.type === "folder") {
-              return a.files.length - b.files.length
-            }
-            return 0
-          });
-          
-          return asc ? sorted : sorted.reverse()
-        }
 
-        if(sortType === "date") {
-          const sorted = [...documents].sort((a, b) => {
-            return new Date(a.added).getTime() - new Date(b.added).getTime()
-          });
-          
-          return asc ? sorted : sorted.reverse()
-        }
+    
+    // return s.map((item) => {
+    //   return item.type === "folder" && item.files ? sortDocuments(item.files, "name", asc) : item 
+    // })
+  }
 
-        return documents
-    }
+  if(sortType === "size") {
+    const sorted = [...documents].sort((a, b) => {
+      if(a.type === "folder" && b.type === "folder") {
+        return a.files.length - b.files.length
+      }
+      return 0
+    });
+    
+    return asc ? sorted : sorted.reverse()
+  }
+
+  if(sortType === "date") {
+    const sorted = [...documents].sort((a, b) => {
+      return new Date(a.added).getTime() - new Date(b.added).getTime()
+    });
+    
+    return asc ? sorted : sorted.reverse()
+  }
+
+  return documents
+}
+
+    // export const sortDocuments = (documents: Documents, sortType: SortType, asc: boolean): Documents => {
+    //     if(sortType === "name") {
+    //       const sorted = [...documents].sort((a, b) => a.name.localeCompare(b.name));
+          
+    //       return asc ? sorted : sorted.reverse()
+    //     }
+
+    //     if(sortType === "size") {
+    //       const sorted = [...documents].sort((a, b) => {
+    //         if(a.type === "folder" && b.type === "folder") {
+    //           return a.files.length - b.files.length
+    //         }
+    //         return 0
+    //       });
+          
+    //       return asc ? sorted : sorted.reverse()
+    //     }
+
+    //     if(sortType === "date") {
+    //       const sorted = [...documents].sort((a, b) => {
+    //         return new Date(a.added).getTime() - new Date(b.added).getTime()
+    //       });
+          
+    //       return asc ? sorted : sorted.reverse()
+    //     }
+
+    //     return documents
+    // }
